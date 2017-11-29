3b260214ece5178ba5e0aa4235ac7af448dad38b
Marek Zielina <niurop@gmail.com>
2017-11-29 18:50:57 +0100

    Add files via upload

--- sketch_nov24a.ino
+++ sketch_nov24a.ino
@@ -1,6 +1,5 @@
 char input[6];
 char state[6];
-char target[6];
 const int speed = 10;
 #include <Servo.h>
 #include <Braccio.h>
@@ -13,23 +12,17 @@ Servo gripper;
 
 void setup() {
   Braccio.begin();
-  state[0] = target[0] = 90;
-  state[1] = target[1] = 90;
-  state[2] = target[2] = 90;
-  state[3] = target[3] = 90;
-  state[4] = target[4] = 90;
-  state[5] = target[5] = 75;
+  state[0] = 90;
+  state[1] = 90;
+  state[2] = 90;
+  state[3] = 90;
+  state[4] = 90;
+  state[5] = 75;
   Serial.begin(9600);
 }
 
 void loop() {
-  state[0] = target[0];
-  state[1] = target[1];
-  state[2] = target[2];
-  state[3] = target[3];
-  state[4] = target[4];
-  state[5] = target[5];
-  Braccio.ServoMovement(0, state[0], state[1], state[2], state[3], state[4], state[5]);
+  Braccio.ServoMovement(20, state[0], state[1], state[2], state[3], state[4], state[5]);
   delay(100);
 }
 
@@ -40,28 +33,34 @@ void serialEvent() {
     while (Serial.available())
       Serial.read();
   }
-  input[0] -= 16;
+  input[0] -= 17;
   if(!(input[0] >= 0 && input[0] <= 180))
-    return;
-  input[1] -= 16;
+    input[0] = state[0];
+  input[1] -= 17;
   if(!(input[1] >= 15 && input[1] <= 165))
-    return;
-  input[2] -= 16;
+    input[1] = state[1];
+  input[2] -= 17;
   if(!(input[2] >= 0 && input[2] <= 180))
-    return;
-  input[3] -= 16;
+    input[2] = state[2];
+  input[3] -= 17;
   if(!(input[3] >= 0 && input[3] <= 180))
-    return;
-  input[4] -= 16;
+    input[3] = state[3];
+  input[4] -= 17;
   if(!(input[4] >= 0 && input[4] <= 180))
-    return;
-  input[5] -= 16;
+    input[4] = state[4];
+  input[5] -= 17;
   if(!(input[5] >= 10 && input[5] <= 73))
-    return;
-  target[0] = input[0];
-  target[1] = input[1];
-  target[2] = input[2];
-  target[3] = input[3];
-  target[4] = input[4];
-  target[5] = input[5];
+    input[5] = state[5];
+  Serial.println(input[0],DEC);
+  Serial.println(input[1],DEC);
+  Serial.println(input[2],DEC);
+  Serial.println(input[3],DEC);
+  Serial.println(input[4],DEC);
+  Serial.println(input[5],DEC);
+  state[0] = input[0];
+  state[1] = input[1];
+  state[2] = input[2];
+  state[3] = input[3];
+  state[4] = input[4];
+  state[5] = input[5];
 }
