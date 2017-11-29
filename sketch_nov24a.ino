ce514bcc5410ad5ebb76fb45d3956b55f9f58222
Marek Zielina <niurop@gmail.com>
2017-11-29 17:36:57 +0100

    Add files via upload

--- sketch_nov24a.ino
+++ sketch_nov24a.ino
@@ -3,6 +3,7 @@ char state[6];
 char target[6];
 const int speed = 10;
 #include <Servo.h>
+#include <Braccio.h>
 Servo base;
 Servo shoulder;
 Servo elbow;
@@ -11,40 +12,25 @@ Servo wrist_ver;
 Servo gripper;
 
 void setup() {
-  base.attach(11);
-  shoulder.attach(10);
-  elbow.attach(9);
-  wrist_ver.attach(6);
-  wrist_rot.attach(5);
-  gripper.attach(3);
-  target[0] = state[0] = 90;
-  target[1] = state[1] = 90;
-  target[2] = state[2] = 90;
-  target[3] = state[3] = 90;
-  target[4] = state[4] = 90;
-  target[5] = state[5] = 73;
+  Braccio.begin();
+  state[0] = target[0] = 90;
+  state[1] = target[1] = 90;
+  state[2] = target[2] = 90;
+  state[3] = target[3] = 90;
+  state[4] = target[4] = 90;
+  state[5] = target[5] = 75;
   Serial.begin(9600);
 }
 
 void loop() {
   state[0] = target[0];
-  base.write(state[0]);
-  
   state[1] = target[1];
-  shoulder.write(state[1]);
-  
   state[2] = target[2];
-  elbow.write(state[2]);
-  
   state[3] = target[3];
-  wrist_ver.write(state[3]);
-  
   state[4] = target[4];
-  wrist_rot.write(state[4]);
-  
   state[5] = target[5];
-  gripper.write(state[5]);
-  delay(1);
+  Braccio.ServoMovement(0, state[0], state[1], state[2], state[3], state[4], state[5]);
+  delay(100);
 }
 
 void serialEvent() {
