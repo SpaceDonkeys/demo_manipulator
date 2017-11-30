c8b58bd8821317d9195f2de4d23cb13036d3b5b4
Mikołaj Karawacki <mapkarawacki@gmail.com>
2017-11-30 12:49:21 +0100

    Node.JS <-> Arduino communications stub added

---
+++ JS/serialport.js
@@ -0,0 +1,17 @@
+﻿var SerialPort = require("serialport");
+var portName = "COM3";
+var serialport = new SerialPort(portName, {
+    baudRate: 9600//,
+    // Look for return and newline at the end of each data packet
+    //parser: SerialPort.parsers.readline("\n")
+});
+
+serialport.on('open', function (err) {
+    console.log('Serial Port open.');
+    serialport.on('readable', function () {
+        console.log(serialport.read().toString());
+    });
+    if (err) {
+        console.log('Error when trying to open:' + err);
+    }
+});
